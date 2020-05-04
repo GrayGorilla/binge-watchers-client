93a6c3c641eb3909141b47ebc18108e1068b5387
Wesley DeVore <wdevo001@ucr.edu>
2020-05-03 19:15:01 -0700

    start of graphics displays

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -8,13 +8,18 @@ import MenuItem from "@material-ui/core/MenuItem"
 import InputLabel from "@material-ui/core/InputLabel"
 import FormControl from "@material-ui/core/FormControl"
 
+var analytics = 0;
+
 class Analysis extends React.Component {
+  //var analytics = "0";
+
   constructor(props) {
     super(props);
     this.state = {
       error: null,
       isLoaded: false,
       selection: null,
+      entries: [],
       textFields: {
         category: null,
         channel: null
@@ -46,11 +51,13 @@ class Analysis extends React.Component {
 
   handleSelectChange(type) {
     const value = type.target.value;
-    console.log(type.target.value);
+    console.log(value);
+    analytics=value;
     this.setState({
       ...this.state,
       selection: value,
     });
+    console.log(analytics);
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -60,9 +67,134 @@ class Analysis extends React.Component {
       textFields,
       selection
     } = this.state;
+
+    // Selection changed
+    if(selection && selection !== prevState.selection) {
+      this.setState({
+        ...this.state,
+        isLoaded: false
+      });
+      // Create Query
+      const params = {
+        "category_id": textFields.category,
+        "channel_title": textFields.channel
+      };
+      // Generate parameters string
+      const esc = encodeURIComponent;
+      let queryList = [];
+
+      for (let key in params) {
+        if(params[key]) {
+          queryList.push(esc(key) + '=' + esc(params[key]))
+        }
+      }
+      const query = queryList.join('&');
+      // Hard coded fetch to server ip
+      console.log('Stuff::', `http://${SERVER_IP}:${SERVER_PORT}/test`)
+      fetch(`http://${SERVER_IP}:${SERVER_PORT}/data?${query}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log('JSON response: ', result);
+          for (let i = 0; i < result.results.length; i++) {
+            result.results[i].unshift(result.resultsIndex[i]);
+          }
+          this.setState({
+            ...this.state,
+            error: null,
+            isLoaded: true,
+            selection: prevState.selection,
+            entries: [],
+            textFields: {
+              category: null,
+              channel: null
+            }
+          });
+        },
+        (error) => {
+          this.setState({
+            ...this.state,
+            error,
+            isLoaded: true,
+            selection: prevState.selection,
+            entries: [],
+            textFields: {
+              category: null,
+              channel: null,
+            }
+          });
+        }
+      )
+    }
   }
 
+  render_data(selected) {
+    console.log('Creating graphic')
+    switch(analytics) {
+      case 1:
+        return(
+          <Button>
+            Buzzword
+          </Button>
+        )
+
+      case 2:
+        return(
+          <Button>
+            Tags
+          </Button>
+        )
+
+      case 3:
+        return(
+          <Button>
+            Day of the Week
+          </Button>
+        )
+
+      case 4:
+        return(
+          <Button>
+            Category
+          </Button>
+        )
+
+      default:
+        return(
+          <div>
+            Here 
+          </div>
+        )
+
+  }
+}
+
   render() {
+    const{error, isLoaded, entries, selection} = this.state;
+    console.log('Rendering...');
+    if(error) {
+      return(
+        <div className='Analysis-header'>
+          <header className = 'Analysis-header'>
+            <h3>
+              Error: {error.message}
+            </h3>
+          </header>
+        </div>
+      )
+    }
+    else if(!isLoaded) {
+      return(
+        <div className='Analysis-header'>
+          <header className = 'Analysis-header'>
+            <h3>
+              Loading...
+            </h3>
+          </header>
+        </div>
+      )
+    }
+    else {
     return (
       <div className='Analysis'>
         <header className='Analysis-header'>
@@ -91,7 +223,7 @@ class Analysis extends React.Component {
               labelId="analysis"
               id="analysis"
               autoWidth
-              //value="selection"
+              value=''
               onChange={event => this.handleSelectChange(event)}
             >
             <MenuItem value={1}>Buzzwords</MenuItem>
@@ -101,8 +233,12 @@ class Analysis extends React.Component {
             </Select>
           </FormControl>
         </div>
+        <div>
+          {this.render_data(this.selection)}
+        </div>
       </div>
     )
+  }
   }
 };
 
